17d4627d44373c2b4ca2aeb4e0a7ff5f239c4b78
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-07-25 23:35:00 +0600

    static code no functionalities added

---
+++ src/hangman/component/WrongLetter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const WrongLetter = () => {
+    return (
+        <>
+            <div class="wrong-letters-container">
+                <div id="wrong-letters"></div>
+            </div>
+
+            
+      
+        </>
+    )
+}
+
+export default WrongLetter
